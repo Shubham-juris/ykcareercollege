31a3d57ab5c7d651ce7d95407ca91f49bee7d088
vishantjurish <vishant.juris@gmail.com>
2025-04-03 11:03:06 +0530

    new com

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import './App.css';
+import Hero from "./components/home/Hero";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <h1 class='text-3xl font-bold underline text-center bg-stone-600 mt-14 py-19'>
+      <h1 class="text-3xl font-bold underline text-center bg-stone-600 mt-14 py-19">
         Hello world!
       </h1>
-      <p class='text-2xl font-bold'>This is a paragraph.</p>
+      <p class="text-2xl font-bold">This is a paragraph.</p>
+      
     </>
   );
 }
---
+++ src/components/home/Hero.jsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <div className="bg-blue-800">
+      <h1 className="p-2 text-red-500">Home</h1>
+    </div>
+  );
+}
